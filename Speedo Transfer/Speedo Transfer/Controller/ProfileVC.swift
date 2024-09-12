44b8637132971a0dfdaf8780a09761ea1d228891
mariaehab01 <maria.ehab2004@gmail.com>
2024-09-12 05:40:47 +0300

    done profile

---
+++ Speedo Transfer/Speedo Transfer/Controller/ProfileVC.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileVC.swift
+//  Speedo Transfer
+//
+//  Created by Maria Ehab on 12/09/2024.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupNavBar()
+        applyGradientBgYellowToRed()  
+        nameLabel.font = UIFont(name: Fonts.semiBoldInter, size: 20)
+    }
+    
+
+    private func setupNavBar() {
+
+        let backButton = UIBarButtonItem()
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        self.navigationController?.navigationBar.tintColor = Colors.backBtnColor
+        let titleLabel = UILabel()
+        titleLabel.text = "Profile"
+        titleLabel.font = UIFont(name: Fonts.mediumInter, size: 20)
+        titleLabel.textColor = Colors.blackGrayColor
+        titleLabel.sizeToFit()
+        self.navigationItem.titleView = titleLabel
+        
+    }
+
+}
