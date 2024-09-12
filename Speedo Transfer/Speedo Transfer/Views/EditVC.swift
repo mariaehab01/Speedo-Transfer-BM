9098467f4e1ccf2a0b2ca3be334fe9bd7421cd4a
Amrellawah <amrellawah2003@gmail.com>
2024-09-12 10:16:06 +0300

    Edit view added

---
+++ Speedo Transfer/Speedo Transfer/Views/EditVC.swift
@@ -0,0 +1,31 @@
+//
+//  EditVC.swift
+//  Speedo Transfer
+//
+//  Created by 1234 on 12/09/2024.
+//
+
+import UIKit
+
+class EditVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
