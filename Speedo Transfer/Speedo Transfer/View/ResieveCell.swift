ccc433118ae8c61e7d24c79f664476dd5623c468
Amrellawah <amrellawah2003@gmail.com>
2024-09-12 11:26:21 +0300

    updates

---
+++ Speedo Transfer/Speedo Transfer/View/ResieveCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResieveCell.swift
+//  Speedo Transfer
+//
+//  Created by 1234 on 12/09/2024.
+//
+
+import UIKit
+
+class ResieveCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
