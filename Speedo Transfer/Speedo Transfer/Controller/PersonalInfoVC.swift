0c328c3edd4ab66865e4ee07ee0cd4764ab9e990
mariaehab01 <maria.ehab2004@gmail.com>
2024-09-12 06:09:51 +0300

    personal info done

---
+++ Speedo Transfer/Speedo Transfer/Controller/PersonalInfoVC.swift
@@ -0,0 +1,35 @@
+//
+//  PersonalInfoVC.swift
+//  Speedo Transfer
+//
+//  Created by Maria Ehab on 12/09/2024.
+//
+
+import UIKit
+
+class PersonalInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupNavBar()
+        applyGradientBgYellowToRed()
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+
+    private func setupNavBar() {
+
+        let backButton = UIBarButtonItem()
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        self.navigationController?.navigationBar.tintColor = Colors.backBtnColor
+        let titleLabel = UILabel()
+        titleLabel.text = "Profile information"
+        titleLabel.font = UIFont(name: Fonts.mediumInter, size: 20)
+        titleLabel.textColor = Colors.blackGrayColor
+        titleLabel.sizeToFit()
+        self.navigationItem.titleView = titleLabel
+        
+    }
+
+}
